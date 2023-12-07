e7fdbe0c939300c77e661baf61749fcd5fea4c33
Jinse <136444748+kadhigfg@users.noreply.github.com>
2023-12-07 17:56:19 +0900

    [level unrated] Title: 문자열 돌리기, Time: 2.75 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for (auto n : str)
+    {
+        cout << n << endl;
+    }
+    
+    return 0;
+}
